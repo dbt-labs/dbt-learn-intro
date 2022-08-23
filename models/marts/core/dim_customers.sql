d4010ba2337c2a189ce206901afabb5469fa8a7a
coapacetic <kyle.d.coapman@gmail.com>
2022-08-23 16:50:01 -0400

    add comments

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -17,7 +17,7 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value
+        -- add your new column here
     from orders
     group by 1
 
@@ -32,7 +32,7 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_value
+        -- add your new column here
     from customers
     left join customer_orders using (customer_id)
 
